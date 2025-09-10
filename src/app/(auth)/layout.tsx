344571ead94a3acd9db578b758fe7178b6679ba3
RashedulHaqueRasel1 <rashedulhaquerasel1@gmail.com>
2025-09-11 01:58:24 +0600

    feat(auth): implement forget password, reset password, and verify OTP functionality
    - Added ForgetPassword component with email validation and OTP sending logic.
    - Created ResetPassword component as a placeholder for future implementation.
    - Developed VerifyOTP component to handle OTP input, verification, and resend functionality.
    - Established layout for authentication pages with responsive design and image integration.
    - Integrated form handling using react-hook-form and zod for validation.

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import "../globals.css";
+import Image from "next/image";
+
+export const metadata: Metadata = {
+  title: "Dive Into Adventure",
+  description: "From beginner to pro, we guide your underwater adventure.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="h-screen flex flex-col-reverse md:flex-row items-center justify-center gap-6 p-4 md:p-0 my-20 md:my-0">
+      {/* Image */}
+      <div className="w-full md:w-1/2 flex justify-center">
+        <Image
+          src={"/images/authImage.png"}
+          width={1024}
+          height={1024}
+          alt="Man Diving"
+          className="w-full h-auto max-h-[300px] md:max-h-full object-cover rounded-lg"
+          priority
+        />
+      </div>
+
+      <div className="w-full md:w-1/2 flex items-center justify-start pl-4 md:pl-2 lg:p-32">
+        {children}
+      </div>
+    </div>
+  );
+}
---
+++ src/app/(auth)/verify-otp/page.tsx
@@ -0,0 +1,10 @@
+import VerifyOTP from '@/components/Auth/VerifyOTP/VerifyOTP'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <VerifyOTP />
+    </div>
+  )
+}
